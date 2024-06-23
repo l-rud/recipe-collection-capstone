4436d95ecdd038c09974a9764f80204557edf6bd
Larysa Rud <larpavrud@gmail.com>
2024-06-22 21:45:02 -0400

    Creating Layout component to organize the application's layout and routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
-import { useState } from 'react'
+import { useState } from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import './App.css'
+import Layout from "./pages/Layout";
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-    <p>{import.meta.env.VITE_apiBaseUrl}</p>
+    <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
 
-export default App
+export default App;
